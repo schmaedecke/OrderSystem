cbfa235c302e5e113d9c32167253ca33de5694cc
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-12-04 14:11:44 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using OrderSystem.Entities;
+using OrderSystem.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace OrderSystem 
+{
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter client data");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string clientEmail = Console.ReadLine();
+            Console.WriteLine("BIrth date (DD/MM/YYYY): ");
+            DateTime clientBirthDate = DateTime.Parse(Console.ReadLine());
+            ;
+            Console.WriteLine("Enter order data");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client clientData = new Client(clientName, clientEmail, clientBirthDate);
+            Order order = new Order(DateTime.Now, status, clientData);
+
+            Console.Write("How many items to this order: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i+1} item data");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product product = new Product(productName, productPrice);
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+                
+                OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+                order.AddItem(orderItem);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
